7e08bd2039109920157a6e858687fecb4245db5d
celenny <celennycs@gmail.com>
2022-01-06 18:38:52 -0300

    index

---
+++ index.js
@@ -0,0 +1,8 @@
+var name = document.querySelector('#exampleInputName'); 
+var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+var birth = ddocument.querySelector('#exampleInputBirth');
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail1');
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector('#exampleInputFile');
+var admin = document.querySelector('#exampleInputAdmin');
