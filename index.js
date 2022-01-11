b22fb87e4e493cfd71895321aff62c68594bf1cd
celenny <celennycs@gmail.com>
2022-01-10 22:48:39 -0300

    class

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {}
 
 function addLine(dataUser) {
-    var tr = document.createElement('tr');
-
-    tr.innerHTML = `
-      <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-      </tr>
-    `;
-
-    document.getElementById('table-users').appendChild(tr);
+    console.log(dataUser);
+    
+    document.getElementById('table-users').innerHTML = `
+        <tr>
+          <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+          <td>${dataUser.name}</td>
+          <td>${dataUser.email}</td>
+          <td>${dataUser.admin}</td>
+          <td>${dataUser.birth}</td>
+          <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+          </td>
+        </tr>
+      `;
 }
 
 document.getElementById('form-user-create').addEventListener('submit', function(event){
@@ -38,5 +36,7 @@ document.getElementById('form-user-create').addEventListener('submit', function(
 
     });
 
-    addLine(user);
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin)
+
+    addLine(objectUser);
 });
