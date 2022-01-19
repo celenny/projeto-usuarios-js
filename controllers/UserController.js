51e0f3837c9e73f27b5ac9a0ab7b91619cda0c3b
celenny <celennycs@gmail.com>
2022-01-18 21:51:50 -0300

    painel editar usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,14 @@ class UserController {
           this.tableEl = document.getElementById(tableId); // element table 
 
           this.onSubmit();
+          this.onEdit();
       } // constructor
+
+      onEdit() {
+          document.querySelector('#box-user-update .btn-cancel').addEventListener('click', e => {
+                this.showPanelCreate();
+          });
+      } // onEdit
 
       onSubmit() {
         // metod p/ enviar formulario
@@ -112,14 +119,31 @@ class UserController {
               <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
               <td>${Utils.dateFormat(dataUser.register)}</td>
               <td>
-                  <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                  <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                   <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
               </td>`;
+
+          tr.querySelector('.btn-edit').addEventListener('click', e=> {
+              console.log(JSON.parse(tr.dataset.user));
+              this.showPanelUpdate();
+          });
 
           this.tableEl.appendChild(tr);
           
           this.updateCount();
       } // addLine
+
+      showPanelCreate() {
+        // mostrar painel de usuários
+        document.querySelector('#box-user-create').style.display = "block";
+        document.querySelector('#box-user-update').style.display = "none";
+      }
+
+      showPanelUpdate() {
+        // mostrar painel de editar usuarios
+        document.querySelector('#box-user-create').style.display = "none";
+        document.querySelector('#box-user-update').style.display = "block";
+      }
 
       updateCount() {
         let numberUsers = 0;
